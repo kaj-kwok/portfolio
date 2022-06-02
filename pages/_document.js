7c360b8db2cc7d2bb78b02a09c7c6924907b3275
Ryan Kwok <kaj.kwok@gmail.com>
2022-06-02 01:05:32 -0700

    changed directory for theme styles on document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from './styles/theme'
-
+import theme from '../lib/styles/theme';
 
 export default class Document extends NextDocument {
   render() {
