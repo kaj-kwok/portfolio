dc109b6201c27047c96722dca671cae488ce6719
Ryan Kwok <kaj.kwok@gmail.com>
2022-06-03 13:55:33 -0700

    animated text to hero

--- components/hero.js
+++ components/hero.js
@@ -7,6 +7,51 @@ const StyledDiv = chakra(motion.div, {
   }
 })
 
+const banner = {
+  initial: {
+    opacity: 1
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delayChildren: 5,
+      staggerChildren: 1,
+    },
+  },
+};
+
+const letters = {
+  initial: {
+    opacity: 0,
+    y: 400
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 3,
+    }
+  }
+};
+
+const AnimateLetters = ({ title }) => (
+  <motion.div
+    variants={banner}
+    initial='hidden'
+    animate='show'
+  >{
+      [...title].map((letter, index) => (
+        <motion.span
+          key={letter + index}
+          variants={letters}
+          initial='initial'
+          animate='animate'
+        >{letter}</motion.span>
+      ))}
+  </motion.div>
+)
+
 const Hero = () => {
   return (
     <StyledDiv
@@ -18,7 +63,9 @@ const Hero = () => {
       justifyContent="center"
       height="95vh"
     >
-      <Text fontSize="20px">Hello there, my name is</Text>
+      {/* <div fontSize="20px"> */}
+      <AnimateLetters title={"Hello there, my name is"} />
+      {/* </div> */}
       <Heading as="h3" fontFamily="heading" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text" fontSize="6xl" pb={2}>Ryan Kwok</Heading>
       <Heading as="h3" fontFamily="heading">I like to build cool things for the Web.</Heading>
       <Text width="50%" my={7} fontSize="20px">I am a full-stack developer located in Vancouver, BC with a passion designing and building user-friendly Web Applications. </Text>
